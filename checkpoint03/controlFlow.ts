34f8862f3a6ef27715924c8fd6ed089c2cc026c3
artisansprince <artisansprince@gmail.com>
2024-09-25 09:26:25 +0700

    🌱 feat: Add checkpoint03 program file.

---
+++ checkpoint03/controlFlow.ts
@@ -0,0 +1,37 @@
+// Control Flow
+
+// If-Else
+let number = 7;
+if (number > 10) {
+  console.log("Number is greater than 10.");
+} else {
+  console.log("Number is 10 or less.");
+}
+
+// Switch Case
+let day = 3;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  default:
+    console.log("Another day");
+}
+
+// For Loop
+for (let i = 0; i < 5; i++) {
+  console.log(`Iteration ${i}`);
+}
+
+// While Loop
+let counter = 0;
+while (counter < 3) {
+  console.log(`Counter: ${counter}`);
+  counter++;
+}
